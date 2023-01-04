896f40cfc1f19e327c5bb21bca8b664156a1e764
jfcal <jfcallem@gmail.com>
2023-01-03 22:42:44 -0400

    Complex prie calculations added, they can be modify in excel

--- main.py
+++ main.py
@@ -9,15 +9,23 @@ def check_num_cotiz(directory):
     for file_name in os.listdir(directory):
         if file_name.endswith(".xlsx"):
             excel_files.append(os.path.join(directory, file_name))
+
     if len(excel_files) > 10:
-        excel_files = excel_files.sort(key=os.path.getmtime)
+        # Sort the Excel files by modified time, oldest first
+        excel_files.sort(key=lambda x: os.path.getmtime(x))
+
+        # Delete the oldest 5 Excel files
         for i in range(5):
             os.remove(excel_files[i])
 
-    # get only the name (no extention file) of the most recent file inside the directory
-    excel_name = os.path.splitext(os.path.basename(excel_files[-1]))[0]
-    num_cotiz = int(excel_name)+1
-    return str(num_cotiz)
+    # Find the next consecutive number to use as a filename
+    max_number = 0
+    for excel_file in excel_files:
+        # Extract the number from the filename
+        number = int(os.path.basename(excel_file).split('.')[0])
+        if number > max_number:
+            max_number = number
+    return str(max_number + 1)
 
 def inset_data_in_excel(ws, pdf_names, pdf_pages, pdf_size):
     items = len(pdf_names)
@@ -38,23 +46,38 @@ def inset_data_in_excel(ws, pdf_names, pdf_pages, pdf_size):
         ws.cell(row=6+i, column=6).value = "Blanco y negro"
         ws.cell(row=6+i, column=7).value = "=M"+str((6+i))
 
-        # Put formulas to calculate the price
+        # Put formulas to calculate price per page and price per finish
+        ws.cell(row=6+i, column=9).value = "Ninguna"
+
+
+
+
+        ws.cell(row=6+i, column=10).value = ("=INDEX(Precios!C5:D7;MATCH(E"+str((6+i))+";Precios!B5:B7;0);MATCH(F"+str((6+i))+";Precios!C4:D4;0))*INDEX(Precios!C10:C12;MATCH(D"+str((6+i))+";Precios!B10:B12;1))*INDEX(Precios!C15:C17;MATCH(I"+str((6+i))+";Precios!B15:B17;0))").replace(";", ",")
+        ws.cell(row=6+i, column=11).value = ("=INDEX(Precios!G5:I16;MATCH(D" + str((6 + i)) + ";Precios!F5:F16;1);MATCH(E" + str((6 + i)) + ";Precios!G4:I4;0))").replace(";", ",")
+
+        # Put formulas to calculate the total price
         ws.cell(row=6+i, column=12).value = "=J"+str((6+i))+"*D"+str((6+i))+"+K"+str((6+i))
         ws.cell(row=6+i, column=13).value = "=ROUND(L"+str((6+i))+", 0)"
 
         #add data validation
         dv_size =DataValidation(type="list", formula1='"Carta, Medio oficio, Oficio"')
         dv_Color = DataValidation(type="list", formula1='"Blanco y negro, Colores"')
+        dv_dificulty = DataValidation(type="list", formula1='"Ninguna, Medio, Alta"')
         ws.add_data_validation(dv_size)
         ws.add_data_validation(dv_Color)
+        ws.add_data_validation(dv_dificulty)
         dv_size.add(ws.cell(row=6+i, column=5))
         dv_Color.add(ws.cell(row=6+i, column=6))
+        dv_dificulty.add(ws.cell(row=6+i, column=9))
 
 
     # Make all the final calculations
     ws.cell(row=items+6, column=7).value = "=SUM(G6:G"+str(items+5)+")"
     ws.cell(row=items+8, column=7).value = "=ROUND(G"+str(items+6)+"*G"+str(items+7)+", 0)"
     ws.cell(row=items+9, column=7).value = "=G" + str(items + 6) + "-G" + str(items+8)
+
+
+
 
 #Open a excel file
 wb = openpyxl.load_workbook(r"D:\Proyectos Programación\Cotizaciones\Plantilla.xlsx")
